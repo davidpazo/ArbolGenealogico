3a2bf93ccda8b34d5bcc13b8c26e55da6eac9a94
GenMyModel <help@genmymodel.com>
2014-11-06 08:53:43 +0000

    commit by GenMyModel using genmymodel: v20141106_085342

---
+++ src/main/java/model/arbolG/Arbol.java
@@ -0,0 +1,60 @@
+package model.arbolG;
+
+
+/**
+ * <!-- begin-user-doc -->
+ * <!--  end-user-doc  -->
+ * @generated
+ */
+
+public class Arbol
+{
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	
+	public PrimeraGeneracion primeraGeneracion;
+	
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	
+	public SegundaGeneracion segundaGeneracion;
+	
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	
+	public TerceraGeneracion terceraGeneracion;
+	
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 */
+	public Arbol(){
+		super();
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	
+	public void main() {
+		// TODO implement me	
+	}
+	
+}
+
